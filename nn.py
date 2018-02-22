22b4a4fe7e197c865e95fe2ea549c6750da535f6
arpit-kalla <ak2242@cornell.edu>
2018-02-22 10:53:31 -0500

    Adding Class Structure

---
+++ nn.py
@@ -0,0 +1,17 @@
+import math
+import random
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple
+from itertools import count
+from copy import deepcopy
+from PIL import Image
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torchvision.transforms as T
+
